eea780a22aca7fc88f1867424762d48a3b7a735b
ss-xxx-x-mm <shubhamsharma8337@gmail.com>
2022-06-30 11:35:44 +0530

    OAuth Init

--- calendar_api/models.py
+++ calendar_api/models.py
@@ -24,16 +24,16 @@ from django.db import models
 # }
 
 class Event(models.Model):
-    kind = models.CharField()
-    etag = models.CharField()
-    summary = models.CharField()
-    description = models.CharField()
+    kind = models.CharField(max_length=100)
+    etag = models.CharField(max_length=100)
+    summary = models.CharField(max_length=100)
+    description = models.CharField(max_length=100)
     updated = models.DateTimeField()
-    time_zone = models.CharField()
-    access_role = models.CharField()
-    next_page_token = models.CharField()
-    next_sync_token = models.CharField()
-    default_reminder_method = models.CharField()
+    time_zone = models.CharField(max_length=100)
+    access_role = models.CharField(max_length=100)
+    next_page_token = models.CharField(max_length=100)
+    next_sync_token = models.CharField(max_length=100)
+    default_reminder_method = models.CharField(max_length=100)
     default_reminder_minutes = models.IntegerField()
 
     def __str__(self) -> str:
--- calendar_api/views.py
+++ calendar_api/views.py
@@ -1,14 +1,24 @@
-import imp
 from django.shortcuts import render
 from rest_framework.views import APIView
+import google.oauth2.credentials
+import google_auth_oauthlib.flow
 # Create your views here.
 
 class GoogleCalendarInitAPIView(APIView):
 
     def post(self, request):
-        pass
+        flow = google_auth_oauthlib.flow.Flow.from_client_secrets_file(
+            'client_secret.json',
+            scopes=['https://www.googleapis.com/auth/calendar.events.readonly']
+            )
+        
+        flow.redirect_uri = 'http://localhost:8000/rest/v1/calendar/redirect'
 
+        authorization_url, state = flow.authorization_url(
+            access_type='offline',
+            include_granted_scopes='true')
 
+        print(authorization_url)
 
 class GoogleCalendarRedirectAPIView(APIView):
 
